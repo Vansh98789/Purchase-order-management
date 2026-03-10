021beef9fc9f096417e657b446a66042da0ca113
Vansh Maheshwari <vanshmaheshwari2004.vm@gmail.com>
2026-03-11 02:18:17 +0530

    Ft: added prisma client logic

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-
+import {prisma} from "../../packages/common/db"
 const app=express();
 
 
---
+++ packages/common/db.ts
@@ -0,0 +1,4 @@
+import { PrismaClient } from '@prisma/client/extension'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+export const prisma = new PrismaClient().$extends(withAccelerate())
