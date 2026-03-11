97534da5d10949ef384ff72fb23a5c537a1421d0
Vansh Maheshwari <vanshmaheshwari2004.vm@gmail.com>
2026-03-11 23:29:03 +0530

    Bug: fix the prisma client to made it work

--- packages/common/db.ts
+++ packages/common/db.ts
@@ -1,4 +1,7 @@
-import { PrismaClient } from '@prisma/client/extension'
+import { PrismaClient } from './generated/prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
+console.log("DATABASE_URL:", process.env.DATABASE_URL)
 
-export const prisma = new PrismaClient().$extends(withAccelerate())
+export const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL,
+} as any).$extends(withAccelerate())
